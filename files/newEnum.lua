24226b8b7e7be47c9c4f5b14290ebd66fc06f694
ColorCorrectional <118882412+ColorCorrectional@users.noreply.github.com>
2023-06-02 16:50:50 +0100

    Updated newEnum.lua

--- files/newEnum.lua
+++ files/newEnum.lua
@@ -1,4 +1,23 @@
-function newEnum(name: string, members: {string})
+--!strict
+
+--[[
+	@func Enum
+	
+	StrictEnum & Enum work exactly the same way but with better checking if a Enum exists
+	or not with an error handler.
+	
+	```lua
+		local kind = newEnum('Message', {'Info', 'Error', 'Warn'})
+		
+		kind.Info
+			>
+			.Name : member[i] ('Info')
+			.Value : members[i] (1)
+			.EnumType : name ('Message')
+	```
+]]
+
+function newStrictEnum(name: string, members: {string})
 	local enum = {} 
 
 	for i, member in members do
@@ -18,4 +37,18 @@ function newEnum(name: string, members: {string})
 				error(`Creating new members in {name} is not allowed!`, 2)
 			end,
 		})
+end
+
+function newEnum(name: string, members: {string})
+	local enum = {}
+	
+	for i, member in members do
+		enum[member] = {
+			Name = member,
+			Value = i,
+			EnumType = name
+		}
+	end
+	
+	return table.freeze(enum)
 end
